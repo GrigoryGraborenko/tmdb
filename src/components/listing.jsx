cfc0795a6f6497456abae50deda0f57c0923b87f
GrigoryGraborenko <grisha.graborenko@fieldql.com>
2019-04-11 20:12:46 +1000

    detail page

--- src/components/listing.jsx
+++ src/components/listing.jsx
@@ -5,8 +5,16 @@ const CreateComponent = require('boc/component')(React);
 
 const moment = require('moment');
 
+import Action from './action.jsx';
+
 export default CreateComponent({ movies: "movie_list" }, {
-    handleMovieNavigate(movie) {
+    getInitialState() {
+        return { search: "" }
+    }
+    ,handleSearchChange(evnt) {
+        this.setState({ search: evnt.target.value });
+    }
+    ,handleMovieNavigate(movie) {
         this.action("movie_view", { movie_id: movie.id });
     }
     ,renderMoviePreview(movie) {
@@ -14,24 +22,55 @@ export default CreateComponent({ movies: "movie_list" }, {
         var release_date = moment(movie.release_date, "YYYY-MM-DD");
 
         // console.log(movie);
+        var score_class = "score ";
+        if(movie.vote_average >= 7.5) {
+            score_class += "good-bg";
+        } else if(movie.vote_average >= 5.0) {
+            score_class += "mediocre-bg";
+        } else if(movie.vote_count > 0) {
+            score_class += "bad-bg";
+        } else {
+            score_class += "hide";
+        }
+
+        // return (
+        //     <a onClick={ this.handleMovieNavigate.bind(this, movie) } key={ movie.id }>
+        //         <img src={ "https://image.tmdb.org/t/p/w500/" + movie.poster_path } />
+        //         <div className={ score_class }>{ Math.floor(movie.vote_average * 10 + 0.5) + "%" }</div>
+        //         <div className="title">{ movie.title }</div>
+        //         <div className="release-date">{ release_date.format("MMMM YYYY") }</div>
+        //     </a>
+        // );
         return (
-            <div onClick={ this.handleMovieNavigate.bind(this, movie) } key={ movie.id }>
+            <Action store={ this.props.store } key={ movie.id } name="movie_view" movie_id={ movie.id } >
                 <img src={ "https://image.tmdb.org/t/p/w500/" + movie.poster_path } />
-                <div className="score">{ Math.floor(movie.vote_average * 10 + 0.5) + "%" }</div>
+                <div className={ score_class }>{ Math.floor(movie.vote_average * 10 + 0.5) + "%" }</div>
                 <div className="title">{ movie.title }</div>
                 <div className="release-date">{ release_date.format("MMMM YYYY") }</div>
-            </div>
+            </Action>
         );
     }
     ,render() {
 
+        /// doing search client-side, because this is more of a front-end challenge
+        /// if you wanted to do proper search, just use this in back-end statelet:
+        ///     TMDBCall('search/movie', { api_key: movie_api.api_key, language: "en-US", query: <input search param> })
         if(this.props.movies) {
-            var movies = this.props.movies.map(this.renderMoviePreview);
+            if(this.state.search.length > 0) {
+                var term = this.state.search.toLowerCase();
+                var movie_list = this.props.movies.filter(function(movie) {
+                    return movie.title.toLowerCase().indexOf(term) !== -1;
+                });
+            } else {
+                var movie_list = this.props.movies;
+            }
+            var movies = movie_list.map(this.renderMoviePreview);
         }
 
         return (
             <div>
                 <h1>Movie listing</h1>
+                <input value={ this.state.search } onChange={ this.handleSearchChange } />
                 <div className="movie-listing">
                     { movies }
                 </div>
--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -3,13 +3,39 @@
 const React = require('react');
 const CreateComponent = require('boc/component')(React);
 
+const moment = require('moment');
+
+import Action from './action.jsx';
+
 export default CreateComponent({ route: "route", movie: "movie", pending: "_pending" }, {
     renderMovieDetails(movie) {
 
         // console.log(movie);
+        var release_date = moment(movie.release_date, "YYYY-MM-DD");
+        var hours = Math.floor(movie.runtime / 60);
+        var minutes = movie.runtime % 60;
+        var runtime = hours + "h";
+        if(minutes) {
+            runtime += " " + minutes + "m";
+        }
 
         return (
-            <div>{ movie.title }</div>
+            <div className="movie-detail">
+                <img src={ "https://image.tmdb.org/t/p/w500/" + movie.backdrop_path } />
+                <Action store={ this.props.store } name="home_page" className="back">
+                    &#8592;
+                </Action>
+                <div className="header">
+                    <img src={ "https://image.tmdb.org/t/p/w500/" + movie.poster_path } />
+                    <h1>{ movie.title }</h1>
+                    <div className="release-date">{ release_date.format("YYYY") } &#183; { Math.floor(movie.vote_average * 10 + 0.5) + "%" } user score</div>
+                    <div className="runtime">{ runtime }</div>
+                </div>
+                <div className="overview">
+                    <h2>Overview</h2>
+                    { movie.overview }
+                </div>
+            </div>
         );
     }
     ,render() {
@@ -23,7 +49,6 @@ export default CreateComponent({ route: "route", movie: "movie", pending: "_pend
 
         return (
             <div>
-                <h1>Specific movie</h1>
                 { details }
             </div>
         );
